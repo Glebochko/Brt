95cf8fadd0e72289ac4dc8586d680d10fc30e3b7
gleb skryabin <glebka.sk@gmail.com>
2019-01-10 12:42:02 +0300

    fix class brt

--- Brt.cpp
+++ Brt.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 
 
-int linecount = 0;
-
 
 class PBRTexception{
     public: PBRTexception(){}
@@ -18,11 +16,57 @@ class PBRTexception{
     
 
 class PBRT{
-    
-    public: PBRT(){
-        PBRTexception exception();
+ 
+    public: 
+    PBRT(){}
+
+    PBRTexception exception;
+    int linecount = 0;
+
+    private: void foutdash(int dashcount, ofstream *fout){
+        for(int j = 0; j < dashcount; j++){
+            *fout << '-';
+            if (j == dashcount - 1){
+                *fout << endl;
+            }
+        }
     }
 
+    private: void recFBC(int amount, int alph[], ofstream *fout, string paspart){
+        string newpaspart;
+
+        for(int charNumber = 1; charNumber <= amount; charNumber++){
+            newpaspart = paspart + (char)alph[charNumber];
+
+            *fout << "amount = " << amount;
+            *fout << "   charNumber = " << charNumber;
+            *fout << "   paspart = " << paspart << endl;
+
+            linecount++;
+            if (linecount * 3 > 1000){
+                exception.oversizedFile(fout);
+            }
+            recFBC(amount, alph, fout, newpaspart);
+        }
+        
+        
+        foutdash(2, fout);
+    }
+
+    public: void foutBrtCombinations(int minAmount, int maxAmount, int alph[], string filename){
+        ofstream fout;
+        fout.open("./" + filename);
+        string paspart;
+        PBRTexception exception();
+        for(int amount = minAmount; amount <= maxAmount; amount++){
+            
+            recFBC(amount, alph, &fout, paspart);
+
+
+        }
+        fout << "EOF";
+        fout.close();
+    }
 
 
 };
@@ -30,60 +74,11 @@ class PBRT{
 
 
 
-
-void foutdash(int dashcount, ofstream *fout){
-    for(int j = 0; j < dashcount; j++){
-        *fout << '-';
-        if (j == dashcount - 1){
-            *fout << endl;
-        }
-    }
-}
-
-void recFBC(int amount, int alph[], ofstream *fout, string paspart, PBRTexception exception){
-    string newpaspart;
-
-    for(int charNumber = 1; charNumber <= amount; charNumber++){
-        newpaspart = paspart + (char)alph[charNumber];
-
-        *fout << "amount = " << amount;
-        *fout << "   charNumber = " << charNumber;
-        *fout << "   paspart = " << paspart << endl;
-
-        linecount++;
-        if (linecount * 3 > 1000){
-            exception.oversizedFile(fout);
-        }
-        recFBC(amount, alph, fout, newpaspart);
-    }
-    
-    
-    foutdash(2, fout);
-}
-
-
-void foutBrtCombinations(int minAmount, int maxAmount, int alph[], string filename){
-    ofstream fout;
-    fout.open("./" + filename);
-    string paspart;
-    PBRTexception exception();
-    for(int amount = minAmount; amount <= maxAmount; amount++){
-        
-        recFBC(amount, alph, &fout, paspart, exception);
-
-
-    }
-    fout << "EOF";
-    fout.close();
-}
-
-
-
-
-
 int main(){
     int alph[] = {1, 2, 3, 4};
-    foutBrtCombinations(3, 5, alph, "file.txt");
+
+    PBRT brt;
+    brt.foutBrtCombinations(3, 5, alph, "file.txt");
 
     return 0;
 }
