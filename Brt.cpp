e35fa63d603ec64832f23b9335ba196c459e251b
gleb skryabin <glebka.sk@gmail.com>
2019-01-10 12:30:57 +0300

    add brt class

--- Brt.cpp
+++ Brt.cpp
@@ -4,30 +4,76 @@
 using namespace std;
 
 
-void recFBC(int amount, int alph[], ofstream fout){
+int linecount = 0;
+
+
+class PBRTexception{
+    public: PBRTexception(){}
+    void oversizedFile(ofstream *fout){
+        *fout << "File too big\nExit\nEOF"; 
+        //*fout.close();
+        exit;
+    } 
+};
     
 
-   for(int charNumber = 1; charNumber <= amount; charNumber++){
-            fout << "amount = " << amount << "   charNumber = " << charNumber << endl;
+class PBRT{
+    
+    public: PBRT(){
+        PBRTexception exception();
     }
-    fout << "----------\n"; 
+
+
+
+};
+
+
+
+
+
+void foutdash(int dashcount, ofstream *fout){
+    for(int j = 0; j < dashcount; j++){
+        *fout << '-';
+        if (j == dashcount - 1){
+            *fout << endl;
+        }
+    }
+}
+
+void recFBC(int amount, int alph[], ofstream *fout, string paspart, PBRTexception exception){
+    string newpaspart;
+
+    for(int charNumber = 1; charNumber <= amount; charNumber++){
+        newpaspart = paspart + (char)alph[charNumber];
+
+        *fout << "amount = " << amount;
+        *fout << "   charNumber = " << charNumber;
+        *fout << "   paspart = " << paspart << endl;
+
+        linecount++;
+        if (linecount * 3 > 1000){
+            exception.oversizedFile(fout);
+        }
+        recFBC(amount, alph, fout, newpaspart);
+    }
+    
+    
+    foutdash(2, fout);
 }
 
 
 void foutBrtCombinations(int minAmount, int maxAmount, int alph[], string filename){
-    //ofstream fout;
-    //fout.open("./" + filename);
-    ofstream fout("./" + filename);
-
+    ofstream fout;
+    fout.open("./" + filename);
+    string paspart;
+    PBRTexception exception();
     for(int amount = minAmount; amount <= maxAmount; amount++){
         
-        recFBC(amount, alph, fout);
+        recFBC(amount, alph, &fout, paspart, exception);
 
-        /*for(int charNumber = 1; charNumber <= amount; charNumber++){
-            fout << "amount = " << amount << "   charNumber = " << charNumber << endl;
-        }
-        fout << "----------\n";*/
+
     }
+    fout << "EOF";
     fout.close();
 }
 
